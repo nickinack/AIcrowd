bf21678404f1b2bac5894e72eb09db633316a5f4
Akhilesh <akleshrailsdev@gmail.com>
2020-04-03 18:26:32 +0530

    Fix leaderboard social share

--- app/assets/javascripts/controllers/leaderboards_controller.js
+++ app/assets/javascripts/controllers/leaderboards_controller.js
@@ -45,3 +45,18 @@ Paloma.controller('Leaderboards', {
     pauseAndPlay();
   }
 });
+
+$(document).ready(function(){
+  $('.social-share').click(function(){
+    var socialSelector;
+    if(this.dataset.site == 'facebook')
+    {
+      socialSelector = $('meta[name="og:image"]');
+    }else if(this.dataset.site == 'twitter')
+    {
+      socialSelector = $('meta[name="twitter:image"]');
+    }
+    socialSelector.attr('content', this.parentElement.dataset.img);
+    return SocialShareButton.share(this);
+  });
+});
