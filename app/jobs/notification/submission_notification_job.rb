bbfd2774bf391bc522161d28c59f02652dfc2506
Akhilesh <akleshrailsdev@gmail.com>
2020-05-25 21:03:52 +0530

    set job

--- app/jobs/notification/submission_notification_job.rb
+++ app/jobs/notification/submission_notification_job.rb
@@ -1,10 +1,8 @@
-module Notification
-  class SubmissionNotificationJob < ApplicationJob
-    queue_as :default
+class Notification::SubmissionNotificationJob < ApplicationJob
+  queue_as :default
 
-    def perform(submission_id)
-      submission = Submission.find(submission_id)
-      NotificationService.new(submission.participant_id, submission, 'submission').call
-    end
+  def perform(submission_id)
+    submission = Submission.find(submission_id)
+    NotificationService.new(submission.participant_id, submission, 'submission').call
   end
 end
